a214db22c187956417f129342ab088ba15c2a6bd
Emeka Ewelike <emekaewelike@gmail.com>
2023-01-28 07:27:59 +0100

    first commit

---
+++ NetCoreAPI.Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace NetCoreAPI.Entities
+{
+    public partial class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string ProductSlug { get; set; }
+        public ICollection<ProductImage> ProductImages { get; set; }
+
+    }
+}
+
---
+++ NetCoreAPI.Entities/ProductImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreAPI.Entities
+{
+    public class ProductImage
+    {
+        public int ProductImageId { get; set; }
+        public string ImagePath { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
